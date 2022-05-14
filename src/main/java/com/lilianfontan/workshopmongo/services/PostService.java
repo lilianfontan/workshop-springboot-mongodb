bf382d5b4420ed21eb084b2367872adb35054e76
lilianfontan <lilian.fontan@gmail.com>
2022-05-14 11:17:42 -0300

    Obtendo POST por id

---
+++ src/main/java/com/lilianfontan/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.lilianfontan.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lilianfontan.workshopmongo.domain.Post;
+import com.lilianfontan.workshopmongo.repository.PostRepository;
+import com.lilianfontan.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não Encontrado"));
+	}
+}
